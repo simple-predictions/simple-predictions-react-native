07c57ab2353e8e55e02499a680fd5f857d2882aa
sol <solomonabrahams100@gmail.com>
2021-02-01 11:01:05 +0000

    homepage added

---
+++ src/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Homepage = () => {
+  return (
+    <Text>test</Text>
+  )
+};
+
+export default Homepage;
--- src/LoggedInNav.jsx
+++ src/LoggedInNav.jsx
@@ -5,7 +5,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MiniLeagues from './MiniLeagues';
 import Predictions from './Predictions';
 import Scoring from './Scoring';
+import Homepage from './Homepage';
 import questionMark from '../assets/question-mark.png';
+import homeIcon from '../assets/home.png';
 import correct from '../assets/correct.png';
 import trophy from '../assets/trophy.png';
 
@@ -18,6 +20,19 @@ const LoggedInNav = () => {
         showIcon: false, labelPosition: 'below-icon', inactiveTintColor: 'black', activeTintColor: '#defc5f', style: { backgroundColor: '#defc5f' }, activeBackgroundColor: 'black', tabStyle: { borderRadius: 15, margin: 2 },
       }}
       >
+        <Tab.Screen
+          options={{
+            // eslint-disable-next-line react/prop-types
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                style={{ height: size, width: size, tintColor: color }}
+                source={homeIcon}
+              />
+            ),
+          }}
+          name="Home"
+          component={Homepage}
+        />
         <Tab.Screen
           options={{
             // eslint-disable-next-line react/prop-types
